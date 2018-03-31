750a21cec5563d0ddc25b8f6764e7f8e1ecafb20
Heidi Hlavinka <hchlav@gmail.com>
2018-03-31 21:56:25 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 
 def key_for_min_value(hash)
   min_key = nil
-  min_value = 0
+  min_value = nil
   hash.each do |name, number|
-    if min_value == 0 || number < min_value
+    if min_value == nil || number < min_value
       min_value = number
       min_key = name
     end
