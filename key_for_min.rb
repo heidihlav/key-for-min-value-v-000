887fa70e3810140499a4b16a2f47e344a7e637f1
Heidi Hlavinka <hchlav@gmail.com>
2018-03-31 21:31:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 
 def key_for_min_value(hash)
   lowest_key = nil
-  lowest_value = 0
+  lowest_value = nil
   hash.each do |name, number|
-    if number < lowest_value
+    if lowest_value == nil || number < lowest_value
       lowest_value = number
       lowest_key = name
     end
